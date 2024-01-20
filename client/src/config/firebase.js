e6e0414d90a7c1386370872e776f863ff2e9059e
YeryunJung <2bites@naver.com>
2024-01-21 02:00:43 +0900

    Init: 파이어베이스 db 익스포트

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -13,3 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
