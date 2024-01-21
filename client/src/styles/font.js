ab7dd85212da47346f517de227a4e22d650d45e1
YeryunJung <2bites@naver.com>
2024-01-21 19:22:44 +0900

    Design: 폰트 디자인 설정

---
+++ client/src/styles/font.js
@@ -0,0 +1,34 @@
+import { css } from "styled-components";
+
+export const global = css`
+  font-family: "Pretendard Variable";
+  font-style: normal;
+  font-weight: normal;
+  font-stretch: normal;
+  text-rendering: optimizelegibility;
+`;
+
+export const title = css`
+  font-size: 48px;
+  font-weight: bold;
+`;
+
+export const subtitle = css`
+  font-size: 32px;
+`;
+
+export const small = css`
+  ${global}
+  font-size: 20px;
+`;
+
+export const normal = css`
+  ${global}
+  font-size: 28px;
+`;
+
+export const btnText = css`
+  ${global}
+  font-size: 30px;
+  font-weight: bold;
+`;
