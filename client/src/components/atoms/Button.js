b98bf822ef5fee92dac58a476c585e8a8f581bdc
YeryunJung <2bites@naver.com>
2024-01-21 22:35:22 +0900

    Feat: 버튼 atom 구현

---
+++ client/src/components/atoms/Button.js
@@ -0,0 +1,43 @@
+import styled, { css } from "styled-components";
+import * as fonts from "../../styles/font";
+
+const blueBtn = css`
+  color: var(--white);
+  background-color: var(--blue-400);
+`;
+
+const big = css`
+  width: 200px;
+  height: 80px;
+`;
+
+const normal = css`
+  width: 160px;
+  height: 70px;
+`;
+
+const sizes = { big, normal };
+
+const ButtonStyle = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+  border: none;
+  ${fonts.btnText}
+  ${(props) => (props.size ? sizes[props.size] : "")}
+  ${(props) =>
+    props.color
+      ? blueBtn
+      : css`
+          color: var(--black);
+          background-color: var(--blue-50);
+        `}
+  cursor: pointer;
+`;
+
+function Button({ children, ...props }) {
+  return <ButtonStyle {...props}>{children}</ButtonStyle>;
+}
+
+export default Button;
--- client/src/components/pages/WikiBoardPage.js
+++ client/src/components/pages/WikiBoardPage.js
@@ -1,7 +1,15 @@
 import Header from "../organisms/Header";
+import Button from "../atoms/Button";
 
 function WikiBoardPage() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Button size="big" color="blueBtn">
+        버튼
+      </Button>
+    </>
+  );
 }
 
 export default WikiBoardPage;
