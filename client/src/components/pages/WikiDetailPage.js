4e9be8b6a02b26daac146e5f403ef1d319a9861d
YeryunJung <2bites@naver.com>
2024-01-23 13:08:57 +0900

    Feat: 단건 게시물 UI 구현

--- client/src/components/pages/WikiDetailPage.js
+++ client/src/components/pages/WikiDetailPage.js
@@ -1,11 +1,56 @@
-import styled from "styled-components";
-import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import * as fonts from "../../styles/font";
 import { getWikiDetail } from "../../api/getWikiDetail";
+import Button from "../atoms/Button";
 
 const Wrapper = styled.div`
-  width: 100%;
-  max-width: 800px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: auto;
+  overflow: hidden;
+`;
+
+const PostTitle = styled.h1`
+  ${fonts.title}
+  width: 60%;
+  height: 45px;
+  border: none;
+  padding: 5px;
+  margin: 10px 10px 20px 10px;
+  color: var(--black);
+  text-align: left;
+`;
+
+const PostContent = styled.div`
+  ${fonts.small}
+  width: 60%;
+  height: 500px;
+  border: 1.5px solid var(--silver-100);
+  border-radius: 10px;
+  padding: 20px;
+  margin: 10px;
+  color: var(--black);
+  text-align: left;
+`;
+
+const BtnWrapper = styled.div`
+  width: 60%;
+  height: 70px;
+  display: flex;
+  justify-content: right;
+  text-align: center;
+  margin: 5px 10px;
+
+  & > button {
+    margin-left: 10px;
+  }
 `;
 
 function WikiDetailPage() {
@@ -25,11 +70,19 @@ function WikiDetailPage() {
     fetchData();
   }, [title]);
 
+  function handleUpdate() {}
+
   return (
     <Wrapper>
       {wiki ? (
         <>
-          <h1>{wiki.title}</h1>
+          <PostTitle>{wiki.title}</PostTitle>
+          <PostContent>{wiki.content}</PostContent>
+          <BtnWrapper>
+            <Button size="normal" color="blueBtn" onClick={handleUpdate}>
+              수정
+            </Button>
+          </BtnWrapper>
         </>
       ) : (
         <p>loading... </p>
